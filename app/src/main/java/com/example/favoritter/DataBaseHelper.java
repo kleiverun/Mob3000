2e0d03dab5c7349aa7a40f8e810220992f418569
kleiverun <78538178+kleiverun@users.noreply.github.com>
2022-11-02 12:41:04 +0100

    Added functionality to see favorites. Can see all favorites. Need to add so that you can only see your favorites.

--- app/src/main/java/com/example/favoritter/DataBaseHelper.java
+++ app/src/main/java/com/example/favoritter/DataBaseHelper.java
@@ -1,10 +1,14 @@
 package com.example.favoritter;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class DataBaseHelper extends SQLiteOpenHelper {
     public static final String FAVORITE_TABLE       = "FAVORITE_TABLE";
@@ -18,16 +22,39 @@ public class DataBaseHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        String createTableStatement = "CREATE TABLE "+ FAVORITE_TABLE + " (" + COLUMN_FAVORITE_ID +
-                                    " INTEGER PRIMARY KEY AUTOINCREMENT, " + COLUMN_USER_ID + "INTEGER, " +
-                                    COLUMN_FAVORITE_NAME + "TEXT )";
+        String createTableStatement = "CREATE TABLE "+ FAVORITE_TABLE +
+                " ( " +COLUMN_FAVORITE_ID+" INTEGER PRIMARY KEY AUTOINCREMENT, "
+                 +COLUMN_USER_ID+" INTEGER, "+COLUMN_FAVORITE_NAME+" TEXT )";
 
         db.execSQL(createTableStatement);
     }
 
     //Will be called whenever the database version changes.
     @Override
-    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {  }
 
+    public List<FavoriteModel> getUserFavorite(){
+        List<FavoriteModel> returnList = new ArrayList<>();
+        String queryString = "SELECT * FROM "+FAVORITE_TABLE;
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery(queryString, null);
+
+        if (cursor.moveToFirst()){
+            do{
+                int favoriteID = cursor.getInt(0);
+                int userID = cursor.getInt(1);
+                String favoriteName = cursor.getString(2);
+
+                FavoriteModel newFavorite = new FavoriteModel(userID,favoriteID,favoriteName);
+                returnList.add(newFavorite);
+            } while (cursor.moveToNext());
+        }
+        else {
+
+        }
+
+        cursor.close();
+        db.close();
+        return returnList;
     }
 }
--- app/src/main/java/com/example/favoritter/MainActivity.java
+++ app/src/main/java/com/example/favoritter/MainActivity.java
@@ -3,12 +3,25 @@ package com.example.favoritter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
-
+    ListView lv_favoriteList;
+    DataBaseHelper dbHelper;
+    ArrayAdapter favoriteArrayAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        super.onCreate(savedInstanceState);
+
+        lv_favoriteList = findViewById(R.id.lv_allFavorites);
+        dbHelper = new DataBaseHelper(MainActivity.this);
+
+        showFavoritesOnListView(dbHelper);
+    }
+    private void showFavoritesOnListView(DataBaseHelper dataBaseHelper2){
+        favoriteArrayAdapter = new ArrayAdapter<FavoriteModel>(MainActivity.this, android.R.layout.simple_list_item_1, dbHelper.getUserFavorite());
+        lv_favoriteList.setAdapter(favoriteArrayAdapter);
     }
 }
